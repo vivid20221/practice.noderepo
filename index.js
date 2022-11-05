72adea3881d4c958f097518bfbcafe2d659b68f1
vivid20221 <julainj9011@gmail.com>
2022-11-04 19:14:25 -0500

    edited prompt again to add some badges

--- index.js
+++ index.js
@@ -1,73 +1,48 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const { title } = require('process');
+const { default: Choices } = require('inquirer/lib/objects/choices');
 
-const generateREADME = ({ Title, Description, Table of Contents, Installation, Usage, Credits, Badges, Features, Links, }) =>
-  `# ${title}
+const generateREADME = ({ title, skills, Description, Installation, Usage, Credits, Badges, Features, Contribute, test, }) =>
+  `# ${title} ${skills}
 
   ## ${Description}
   
-  Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
   
-  - What was your motivation?
-  - Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
-  - What problem does it solve?
-  - What did you learn?
-  
-  ## ${Table of Contents} (Optional)
-  
-  If your README is long, add a table of contents to make it easy for users to find what they need.
-  
-  - [Installation](#installation)
-  - [Usage](#usage)
-  - [Credits](#credits)
-  - [License](#license)
+
   
   ## ${Installation}
   
-  What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
+ 
   
   ## ${Usage}
   
-  Provide instructions and examples for use. Include screenshots as needed.
-  
-  To add a screenshot, create an (assets/images) folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
+ 
   
       
   
   ## ${Credits}
   
-  List your collaborators, if any, with links to their GitHub profiles.
   
-  If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
-  
-  If you followed tutorials, include links to those here as well.
-  
-  ## License
-  
-  The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
   
   ---
   
-  🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
-  
+ 
   ## ${Badges}
   
-  ![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
   
-  Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
   
   ## ${Features}
   
-  If your project has a lot of features, list them here.
+  
   
   ##${Contribute}
   
-  If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
+ 
   
-  ## Tests
+  ## ${test} `
   
-  Go the extra mile and write tests for your application. Then provide examples on how to run them here.`;
+  
 
 inquirer
   .prompt([
@@ -77,15 +52,20 @@ inquirer
       message: ' <Your-Project-Title>',
     },
     {
+      type: 'checkbox',
+      name: 'skills',
+      message: 'What coding language did you use?',
+      choices: [ 
+         'https://img.shields.io/badge/Node.js-43853D?style=for-the-badge&logo=node.js&logoColor=white', 'https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black',
+
+      ],
+    },
+    {
       type: 'input',
       name: 'Description',
       message: 'Provide a short description explaining the what, why, and how of your project.',
     },
-    {
-      type: 'input',
-      name: ' Table of Contents',
-      message: 'add a table of contents to make it easy for users to find what they need.',
-    },
+   
     {
       type: 'input',
       name: 'Installation',
@@ -110,6 +90,13 @@ inquirer
       type: 'input',
       name: 'Features',
       message: 'What features do you want to include?'
+    },
+    {
+      type: 'confirm',
+      name: 'test',
+      message: 'Do you want to run tests?',
+      default: [],
+
     },
     {
       type: 'input',
