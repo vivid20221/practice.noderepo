2811e68db02d1a50a1b5e6fd961ac6216dc2c7ff
vivid20221 <julainj9011@gmail.com>
2022-11-06 17:09:42 -0600

    fixed the unifined lines

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const { title } = require('process');
-const { default: Choices } = require('inquirer/lib/objects/choices');
+const  title = require('process');
+const choices = require('inquirer/lib/objects/choices');
 
 const generateREADME = ({ title, skills, Description, Installation, Usage, Credits, Badges, Features, Contribute, test, }) =>
-  `# ${title} ${skills}
+  ` ${skills}
+  # ${title}
 
   ## ${Description}
   
@@ -40,7 +41,7 @@ const generateREADME = ({ title, skills, Description, Installation, Usage, Credi
   
  
   
-  ## ${test} `
+  ## ${test} `;
   
   
 
@@ -57,7 +58,7 @@ inquirer
       message: 'What coding language did you use?',
       choices: [ 
          'https://img.shields.io/badge/Node.js-43853D?style=for-the-badge&logo=node.js&logoColor=white', 'https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black',
-
+          'https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white','	https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white','https://img.shields.io/badge/JavaScript-323330?style=for-the-badge&logo=javascript&logoColor=F7DF1E','https://img.shields.io/badge/Python-14354C?style=for-the-badge&logo=python&logoColor=white', 'https://img.shields.io/badge/Express.js-404D59?style=for-the-badge','ttps://img.shields.io/badge/MySQL-00000F?style=for-the-badge&logo=mysql&logoColor=white',
       ],
     },
     {
@@ -95,7 +96,7 @@ inquirer
       type: 'confirm',
       name: 'test',
       message: 'Do you want to run tests?',
-      default: [],
+      default: [false],
 
     },
     {
@@ -105,7 +106,7 @@ inquirer
     }
   ])
   .then((answers) => {
-    const readmePageContent = generateREADME(answers);
+    const readmePageContent = generateREADME(answers, answers.skills);
 
     fs.writeFile('README.md', readmePageContent, (err) =>
       err ? console.log(err) : console.log('Successfully created README.md!')
